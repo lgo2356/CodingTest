675ca0c21e900cbf7fcd2c6a42fa1037b568446e
Jeon <wmf85276@gmail.com>
2021-04-24 23:27:37 +0900

    1, 2, 3 더하기 #9095

--- 123plus_9095.c
+++ 123plus_9095.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
+int memo[1001];
+
+int dp(int x)
+{
+    if (x == 0)
+        return 0;
+    if (x == 1)
+        return 1;
+    if (x == 2)
+        return 2;
+    if (x == 3)
+        return 4;
+
+    if (memo[x] != 0)
+        return memo[x];
+
+    memo[x] = dp(x - 3) + dp(x - 2) + dp(x - 1);
+
+    return memo[x];
+}
+
 int main(void)
 {
-    printf("Hello world!\n");
+    int T, x;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++)
+    {
+        scanf("%d", &x);
+        printf("%d\n", dp(x));
+    }
+
     return 0;
 }
